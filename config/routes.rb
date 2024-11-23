3bf9b86ff339ec1d02f6a8f200e498cc04b1c3a4
Xin shen <363088847@qq.com>
2024-11-23 13:59:18 +0000

    user demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/show"
+  get "users/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+  get "/users/:id", to: "users#show"
+  post "/users/", to: "users#create"
 end
